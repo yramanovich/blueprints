ffc1aeaf98babeef0141a29ed85586df24d7e10e
Yauheni Ramanovich <yauheni.ramanovich@gmail.com>
2024-01-26 17:37:09 +0100

    Add monotonous stack example

--- stack.go
+++ stack.go
@@ -7,3 +7,16 @@ func (st *stack[T]) len() int    { return len(*st) }
 func (st *stack[T]) peek() T     { return (*st)[len(*st)-1] }
 func (st *stack[T]) push(e T)    { *st = append(*st, e) }
 func (st *stack[T]) pop() T      { x := (*st)[len(*st)-1]; *st = (*st)[:len(*st)-1]; return x }
+
+// MonotonousStack shows an example how to use monotonous increase stack.
+func MonotonousStack(nums []int) []int {
+	st := stack[int]{}
+
+	for i := 0; i < len(nums); i++ {
+		for !st.empty() && st.peek() > nums[i] {
+			st.pop()
+		}
+		st.push(nums[i])
+	}
+	return st
+}
--- stack_test.go
+++ stack_test.go
@@ -1,6 +1,7 @@
 package blueprints
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -15,4 +16,10 @@ func TestStack(t *testing.T) {
 	assert.NotPanics(t, func() { numsStack.push(4) })
 	assert.Equal(t, 4, numsStack.pop())
 	assert.Equal(t, 3, numsStack.len())
+}
+
+func TestMonotonousStack(t *testing.T) {
+	nums := []int{3, 7, 8, 4, 5, 9, 1, 2, 11}
+	monotonous := MonotonousStack(nums)
+	fmt.Println(monotonous)
 }
